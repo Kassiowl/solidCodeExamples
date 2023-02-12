3a0dfc9fbe36e340371bcff9d7d47f8b50aaa6bc
Kassio Wallace <kassiowl34@gmail.com>
2023-02-12 13:32:06 -0300

    lsp mod

--- lsp/lsp.js
+++ lsp/lsp.js
@@ -17,21 +17,23 @@ class EngineVehicles
         throw new Error("Fuel not implemented")
     }
 
+
+
 }
 
 
 class NonEngineVehicles
 {
-    constructor(name, number_of_wheels, vehicle_type)
+    constructor(name, vehicle_type)
     {
         this.name = name
-        this.number_of_wheels = number_of_wheels
         this.vehicle_type = vehicle_type
     }
 
     get_name(){ return this.name    }
     get_number_of_wheels(){ return this.number_of_wheels    }
     get_vehicle_type(){ return this.vehicle_type    }
+    get_number_of_wheels(){     throw new Error("Number of wheels not implemented!")  }
 }
 
 
@@ -61,4 +63,35 @@ class Pickup extends EngineVehicles
         return 'Diesel'
     }
 }
+
+
+class Bike extends NonEngineVehicles
+{
+    get_number_of_wheels(){     return 2    }
+}
+
+class Skateboard extends NonEngineVehicles
+{
+    get_number_of_wheels(){     return 3    }
+}
+
+
+firstVehicle = new Car('2jz', '330')
+secondVehicle = new Car('Pentastar', '285')
+
+thirdVehicle = new Skateboard('Klunker 3000','2', 'offroad')
+
+fourthVehicle = new Skateboard('Wood light', '4', 'city')
+
+console.log(thirdVehicle.get_vehicle_type()) //return Offroad
+
+
+
+console.log(firstVehicle.get_fuel_type()) //Get fuel type normally 
+console.log(secondVehicle.get_fuel_type()) //Get fuel type normally
+
+console.log(firstVehicle.start_engine()) //Start Engine normally
+console.log(secondVehicle.start_engine()) //Start Engine normally
+
+
 
